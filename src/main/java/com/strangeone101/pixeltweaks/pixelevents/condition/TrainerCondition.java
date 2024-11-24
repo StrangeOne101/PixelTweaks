e3205c2f3b76d11b021c942db50a0390f6d602f9
JanoChacon <a.chacon01@ufromail.cl>
2024-11-24 20:38:40 -0300

    fix | trainer music condition multiplayer
    A function was added to validate that one of the participants is a Trainer. If so, it returns the Trainer entity closest to the Pokémon (the Pokémon is generated at the same coordinates as the Trainer).

--- src/main/java/com/strangeone101/pixeltweaks/pixelevents/condition/TrainerCondition.java
+++ src/main/java/com/strangeone101/pixeltweaks/pixelevents/condition/TrainerCondition.java
@@ -5,9 +5,18 @@ import com.pixelmonmod.pixelmon.api.storage.StoragePosition;
 import com.pixelmonmod.pixelmon.api.storage.TrainerPartyStorage;
 import com.pixelmonmod.pixelmon.battles.controller.BattleController;
 import com.pixelmonmod.pixelmon.battles.controller.participants.BattleParticipant;
+import com.pixelmonmod.pixelmon.battles.controller.participants.ParticipantType;
+import com.pixelmonmod.pixelmon.client.ClientProxy;
+import com.pixelmonmod.pixelmon.client.gui.battles.ClientBattleManager;
 import com.pixelmonmod.pixelmon.entities.npcs.NPCTrainer;
+import com.pixelmonmod.pixelmon.entities.npcs.registry.BaseTrainer;
 import com.pixelmonmod.pixelmon.entities.pixelmon.PixelmonEntity;
+import com.strangeone101.pixeltweaks.PixelTweaks;
 import com.strangeone101.pixeltweaks.pixelevents.Condition;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 
 import java.util.List;
 
@@ -22,34 +31,71 @@ public class TrainerCondition extends Condition<NPCTrainer> {
 
     @Override
     public boolean conditionMet(NPCTrainer trainer) {
+
         if (trainer == null) return false; //Not a trainer owned battle
 
 
-        if (trainerType != null && !trainerType.isEmpty() && !trainer.getBaseTrainer().name.equalsIgnoreCase(trainerType)) return false;
-        if (textureIndex != null && trainer.getTextureIndex() != textureIndex) return false;
         if (customSteveTexture != null && !customSteveTexture.isEmpty() && !trainer.getCustomSteveTexture().equals(customSteveTexture)) return false;
         if (name != null && !name.isEmpty() && trainer.getName() != null && !trainer.getName().getString().equals(name)) return false;
-        if (gymLeader != null && trainer.isGymLeader != gymLeader) return false;
-
+        if (gymLeader != null && isGymLeaderAlt(trainer) != gymLeader) return false;
+        if (trainerType != null && !trainerType.isEmpty() && !trainer.getBaseTrainer().name.equalsIgnoreCase(trainerType)) return false;
+        if (textureIndex != null && trainer.getTextureIndex() != textureIndex) return false;
         if (trainer.getTrainerLevel() < minPartyLevel) return false;
         if (trainer.getTrainerLevel() > maxPartyLevel) return false;
-
         return true;
+    }
+
+    public boolean isGymLeaderAlt(NPCTrainer trainer){
+        ItemStack[] winnings =  trainer.getWinnings();
+
+        return false;
     }
 
     @Override
     public NPCTrainer itemFromPixelmon(PixelmonEntity entity) {
         BattleController bt = entity.battleController;
-        List<BattleParticipant> participants = bt.participants;
-        for (BattleParticipant p : participants) {
-            if (p.getEntity() instanceof NPCTrainer){
-                return (NPCTrainer) p.getEntity();
+        if (bt != null) {
+            for (BattleParticipant p : bt.participants) {
+                Entity participantEntity = p.getEntity();
+                if (participantEntity instanceof NPCTrainer) {
+                    return (NPCTrainer) participantEntity;
+                }
+            }
+        } else {
+            ClientBattleManager bm = ClientProxy.battleManager;
+            for (ParticipantType[] type : bm.battleSetup) {
+                if (type[0] == ParticipantType.Trainer) {
+                    Iterable<Entity> entities = Minecraft.getInstance().world.getAllEntities();
+                    Entity closest = findClosestEntityToBlockPos(entities, entity.getPosition(), entity);
+
+                    if (closest instanceof NPCTrainer) {
+                        return (NPCTrainer) closest;
+                    }
+                    break;
+                }
             }
         }
-/*        if (entity.getOwner() instanceof NPCTrainer) {
-            return (NPCTrainer) entity.getOwner();
-        }*/
         return null;
+    }
+
+
+    public Entity findClosestEntityToBlockPos(Iterable<Entity> entities, BlockPos position, Entity exclude) {
+        double minDistanceSquared = Double.MAX_VALUE;
+        Entity closest = null;
+
+        for (Entity e : entities) {
+            if (e.equals(exclude)) {
+                continue;
+            }
+
+            double distanceSquared = e.getDistanceSq(position.getX(), position.getY(), position.getZ());
+            if (distanceSquared < minDistanceSquared) {
+                minDistanceSquared = distanceSquared;
+                closest = e;
+            }
+        }
+
+        return closest;
     }
 
     @Override
